205cbe9f78437c1763db0d3f0beddd1659f68c35
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 11:38:47 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if successful, non-zero value if an error occurred
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
