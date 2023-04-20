3596fc186983dc31f17eb20a33076aab21d80202
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 13:42:17 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if successful, non-zero value if an error occurred
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
