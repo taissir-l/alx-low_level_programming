d482fafec7dd40066bb1ceac2dffec96a9cae0d5
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 10:00:10 -0700

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - the c program
+ * 
+ * retern: 0 (Success)
+ */
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
