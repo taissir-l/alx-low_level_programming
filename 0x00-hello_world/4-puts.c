34eba420605ebfe8699d5ae578fae38321391494
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 11:43:59 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - Entry point of the program
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
