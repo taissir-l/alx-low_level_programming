c8a07fe4317d91946e14c87beca8a5fc623a3d86
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 10:05:17 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * retern: 0 (Success)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
