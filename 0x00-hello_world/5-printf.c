b30035bc18f2e42b1d04c5325a0d98dacca6ce9e
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 13:03:48 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if successful, non-zero value if an error occurred
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
