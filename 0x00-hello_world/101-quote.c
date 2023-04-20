bdbed9b076a105ad6ea74e94d8dc7e6583ff4d4b
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-20 13:49:51 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <unisitd.h>
 #include <stdio.h>
 
 /**
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
