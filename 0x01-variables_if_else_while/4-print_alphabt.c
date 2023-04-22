c1b1a4876fe65aa6c81c9083d1750fbeaf83bf70
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-21 17:18:38 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - the alphabet without q and e
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 132; i++)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+}
+putchar("\n");
+return (0);
+}
