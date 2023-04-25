8389ae1db02444d4b30ad97d27f1a09cf63e3a99
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-25 13:11:17 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - the absolute value of an integer
+ *
+ * @i: integer
+ *
+ * Return: absolut value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
