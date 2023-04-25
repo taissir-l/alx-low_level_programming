a5617ca3d5d24ffa7f99e4559185db72fbfdabdd
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-25 14:07:11 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,7 +20,7 @@ void jack_bauer(void)
 					if (a >= 2 && b >= 4)
 						break;
 					_putchar(a + 48);
-					_putchar(b + 48)
+					_putchar(b + 48);
 					_putchar(58);
 					_putchar(c + 48);
 					_putchar(d + 48);
