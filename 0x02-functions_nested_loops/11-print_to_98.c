ce9b87e7ef2aeb1f446fb3754dc53a2b065623fc
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-25 16:56:29 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	int i, j;
 
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
@@ -22,7 +22,7 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	}
-	else if (n > 98)
+	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
