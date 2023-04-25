aaee335ec284b1990accd72a4f62747cee68f3c3
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-25 13:23:37 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @i: the integer
+ *
+ * Return: last digit 
+ */
+int print_last_digit(int i)
+{
+	int a;
+
+	a = i % 10;
+	if (a < 0)
+	{
+		_putchar (-a + 48);
+		return (-a);
+	}
+	else
+	{
+		_putchar(a + 48);
+		return (a);
+	}
+}	
