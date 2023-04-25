fcae2d6f8738261f85e4e52e053b785f171b4d6a
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-25 10:47:06 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the lowercase alphabet then a new line
+ *
+ * Return: 0 always
+ */
+void print_alphabet_x10(void);
+{
+int a;
+int b;
+for (b = 1; a <= 10; a++)
+{
+for (b = 97; b <= 122; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower -  function that checks for lowercase character
+ *
+ * Return: 1 for lowercase characters and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && C <= 122)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	_putchar('\n');
+}
