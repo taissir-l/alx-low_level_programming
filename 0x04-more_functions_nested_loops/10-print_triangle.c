e2cd11493081d3140acb9d001c7eb4b837ad91c2
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-27 09:11:30 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,11 +15,11 @@ int i, j;
 
 for (i = 1; i <= size; i++)
 {
-for (j = i; j <= size; j++)
+for (j = size - i; j > 0; j--)
 {
 _putchar(' ');
 }
-for (j = 1; j <= i; j++)
+for (j = 0; j < i; j++)
 {
 _putchar('#');
 }
