373b7ce56bdc29428c8febd2739574720bec7ffa
“taissir-l” <taissirlassoued1@gmail.com>
2023-04-27 12:06:33 -0700

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -5,13 +6,18 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
 /*}*/
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
 
