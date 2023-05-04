08bf0e91b7598bb85111acb6a5909f7cd374235f
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-04 07:11:50 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,14 @@
  *
  * @src: second string
  *
- * Return: dest
+ * Return: the concatenated string
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
