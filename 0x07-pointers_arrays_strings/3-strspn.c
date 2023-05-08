fa5a5c3361e92479449228a53be7e10218b72304
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-08 06:42:46 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ *
+ * @s: the input
+ * @accept: the input
+ *
+ * Return: 0 always
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, x = 0;
+
+	while (accept[i])
+	{
+		for (; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				x++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (0);
+		}
+		i++;
+	}
+	return (x);
+}
