4fa448a8ed3d4201d3b20f778221a403da1dad59
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-08 05:40:47 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int a;
 
-	for (; n > 0; i++)
+	for (a = 0; a < n; a++)
 	{
-		s[i] = b;
-		n--;
+		s[a] = b;
 	}
+
 	return (s);
 }
