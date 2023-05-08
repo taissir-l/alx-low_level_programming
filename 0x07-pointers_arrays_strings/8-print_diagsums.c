9d3ccec46cdea191b11a73f9fe0ed4332f7dda63
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-08 10:38:24 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,9 +17,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		s1 += a[i * size + i];
+		s1 += a[(size + 1) * i];
+		s2 += a[(size - 1) * (i + 1)];
 	}
-	for (i = size - 1; i >= 0; i--)
-		s2 += a[i * size + (size - i - 1)];
 	printf("%d, %d\n", s1, s2);
 }
