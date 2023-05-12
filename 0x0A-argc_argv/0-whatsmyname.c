0abc76814eb919b449267aac39071ad593fcb636
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-12 10:48:38 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main -  program that prints its name, followed by a new line
+  * @argc: the argument count
+  * @argv: the vector
+  *
+  * Return: 0 Always
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
