dc55e16d741db3ea8f246c933cec8ed62dad3f13
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-22 10:44:11 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - basic informtion about the dog
+ *
+ * @name: the dog's name
+ * @age : the dog's age
+ * @owner: the dog's owner.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
