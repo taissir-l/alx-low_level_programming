c64461df6dd6c05a8f1cd4561224bec917489b14
“taissir-l” <taissirlassoued1@gmail.com>
2023-06-06 06:29:51 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+/**
+ * free_listint2 - an other function that frees a linked list.
+ *
+ * @head: double pointer to the listint_t list to be freed
+ *
+ * Return: void.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		p = (*head)->next;
+		free(*head);
+		*head = p;
+	}
+
+	*head = NULL;
+}
