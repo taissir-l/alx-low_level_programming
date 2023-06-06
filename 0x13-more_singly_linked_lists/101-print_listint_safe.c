c714a2457270948103d6a7268f51e061d72b7818
“taissir-l” <taissirlassoued1@gmail.com>
2023-06-06 12:39:42 -0700

    task12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,34 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdio.h>
+
+/**
+ * _list - reallocates the memory
+ *
+ * @l: old list
+ * @size: new list size
+ * @new: new node
+ *
+ * Return: pointer to the new list.
+ */
+const listint_t **_list(const listint_t **l, size_t size, const listint_t *new)
+{
+	const listint_t **newl;
+	size_t i;
+
+	newl = malloc(size * sizeof(listint_t *));
+	if (newl == NULL)
+	{
+		free(l);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newl[i] = l[i];
+	newl[i] = new;
+	free(l);
+	return (newl);
+}
+
 
 /**
  * print_listint_safe - function that prints a listint_t linked list.
@@ -10,28 +39,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *c;
-	size_t i;
-	const listint_t *h;
+	size_t i, num = 0;
+	const listint_t **l = NULL;
 
-	c = head;
-	if (c == NULL)
-		exit(98);
-
-	i = 0;
-	while (c != NULL)
+	while (head != NULL)
 	{
-		h = c;
-		c = c->next;
-		i++;
-		printf("[%p] %d\n", (void *)h, h->n);
-
-		if (h < c)
+		for (i = 0 ; i < num; i++)
 		{
-			printf("-> [%p] %d\n", (void *)c, c->n);
-			break;
+			if (head == l[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(l);
+				return (num);
+			}
 		}
+		num++;
+		l = _list(l, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
+	free(l);
 
-	return (i);
+	return (num);
 }
