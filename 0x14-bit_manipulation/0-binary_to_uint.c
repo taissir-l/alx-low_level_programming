77e54dca671ba5eb880378041a19f31654fb4236
“taissir-l” <taissirlassoued1@gmail.com>
2023-11-24 11:44:49 -0800

    bit manip

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_uint - function that converts a binary number
+ *                 of unsigned int.
+ *
+ * @b: string
+ *
+ * Return: converted number
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int val = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		val = 2 * val + (b[i] - '0');
+	}
+	return (val);
+}
